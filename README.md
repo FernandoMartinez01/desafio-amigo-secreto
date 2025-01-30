7b66972c18a3a26aded018be15f6340721b5f622
Fernando Martinez Chehda <fernandomch42@gmail.com>
2025-01-30 02:40:07 -0300

    Se crea aplicación de amigo secreto

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# desafio-amigo-secreto
+# Desafío de Amigo Secreto
+
+El desafío consiste en crear una aplicación que tiene como finalidad elegir un amigo aleatorio a partir de una lista proporcionada por el usuario.
+
+## Funcionalidades del Proyecto
+
+1. **Crear una lista de amigos**  
+   El usuario puede agregar amigos a la lista escribiendo sus nombres.
+
+2. **El programa elige aleatoriamente un amigo de la lista**  
+   El sistema selecciona un amigo aleatoriamente y lo muestra al usuario.
+
+## Tecnologías Utilizadas
+
+- **JavaScript**  
+  Para la lógica de la aplicación y la interacción con el DOM.
+
+- **HTML**  
+  Para la estructura básica de la página.
+
+- **CSS**  
+  Para el diseño visual y la disposición de los elementos en la interfaz.
+
+## Desarrollador de la Solución
+
+**Fernando Martinez Chehda**  
+Creador de la solución al desafío.
--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let cantidad_amigos = amigos.length;
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre");        
+    } else {
+        amigos.push(amigo);
+        cantidad_amigos = amigos.length;
+        actualizarListaAmigos();
+    }
+
+    limpiarCaja();
+    console.log(amigos);
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
+function sortearAmigo() {
+    if (verificaListaVacia()){
+        asignarTextoElemento('resultado', `Amigo sorteado: ${seleccionarAmigo()}`);
+    }
+}
+
+function verificaListaVacia() {
+    if (amigos.length == 0) {
+        alert("Debes agregar al menos un amigo");
+        return false
+    } else {
+        return true
+    }
+}
+
+function generarIndiceAleatorio() {
+    return Math.floor(Math.random() * cantidad_amigos);
+}
+
+function seleccionarAmigo() {
+    return amigos[generarIndiceAleatorio()];
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
+}
